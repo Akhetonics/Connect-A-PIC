7c852cf973cc90c547dc0a19248be92554e69994
Michael Kissner <m1o1d1@gmail.com>
2023-06-30 22:23:08 +0200

    Setting up Godot

---
+++ Scenes/MainMenu.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	private void _on_new_editor_pressed()
+	{
+		var nextScene = (PackedScene)ResourceLoader.Load("res://Scenes/PICEditor.tscn");
+		GetTree().ChangeSceneToPacked(nextScene);
+	}
+}
+
+
