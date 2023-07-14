2f5b59a7c46a5458df17138374a6ffce0de5e376
Michael Kissner <m1o1d1@gmail.com>
2023-07-14 11:03:09 +0200

    Always push to Master

--- Scenes/Components/ComponentGrid.cs
+++ Scenes/Components/ComponentGrid.cs
@@ -3,113 +3,113 @@ using System.Collections.Generic;
 
 public class ComponentGrid
 {
-    private class ComponentGridEntry
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public ComponentItem Comp { get; set; }
-        public int CompOrientation { get; set; }
-        public int CompOffsetX { get; set; }
-        public int CompOffsetY { get; set; }
-    }
+	private class ComponentGridEntry
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public ComponentItem Comp { get; set; }
+		public int CompOrientation { get; set; }
+		public int CompOffsetX { get; set; }
+		public int CompOffsetY { get; set; }
+	}
 
-    public int SizeX { get; private set; }
-    public int SizeY { get; private set; }
+	public int SizeX { get; private set; }
+	public int SizeY { get; private set; }
 
-    private List<ComponentItem> _components = new List<ComponentItem>();
+	private List<ComponentItem> _components = new List<ComponentItem>();
 
-    private ComponentGridEntry[,] _grid;
+	private ComponentGridEntry[,] _grid;
 
-    public ComponentGrid(int sizeX, int sizeY)
-    {
-        this.SizeX = sizeX;
-        this.SizeY = sizeY;
+	public ComponentGrid(int sizeX, int sizeY)
+	{
+		this.SizeX = sizeX;
+		this.SizeY = sizeY;
 
-        this._grid = new ComponentGridEntry[this.SizeX, this.SizeY];
+		this._grid = new ComponentGridEntry[this.SizeX, this.SizeY];
 
-        // Pre-fill grid coordinates
-        for (int i=0; i<sizeX; i++)
-        {
-            for (int j=0; j<sizeY; j++)
-            {
-                this._grid[i, j].X = i;
-                this._grid[i, j].Y = j;
-            }
-        }
-    }
+		// Pre-fill grid coordinates
+		for (int i=0; i<sizeX; i++)
+		{
+			for (int j=0; j<sizeY; j++)
+			{
+				this._grid[i, j].X = i;
+				this._grid[i, j].Y = j;
+			}
+		}
+	}
 
-    public bool checkCollision(int x, int y, int sizeX, int sizeY)
-    {
-        if (x < 0 || y < 0 || (x + sizeX - 1) >= this.SizeX || (y + sizeY - 1) > this.SizeY)
-        {
-            // Out of Bounds
-            return true;
-        }
+	public bool checkCollision(int x, int y, int sizeX, int sizeY)
+	{
+		if (x < 0 || y < 0 || (x + sizeX - 1) >= this.SizeX || (y + sizeY - 1) > this.SizeY)
+		{
+			// Out of Bounds
+			return true;
+		}
 
-        for (int i = x; i < (x + sizeX - 1); i++)
-        {
-            for (int j = y; j < (y + sizeY - 1); j++)
-            {
-                if (this._grid[i, j].Comp != null)
-                {
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
+		for (int i = x; i < (x + sizeX - 1); i++)
+		{
+			for (int j = y; j < (y + sizeY - 1); j++)
+			{
+				if (this._grid[i, j].Comp != null)
+				{
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
 
-    public ComponentItem getComponentAt(int x, int y)
-    {
-        if (x < 0 || y < 0 || x >= this.SizeX || y > this.SizeY)
-        {
-            return null;
-        }
+	public ComponentItem getComponentAt(int x, int y)
+	{
+		if (x < 0 || y < 0 || x >= this.SizeX || y > this.SizeY)
+		{
+			return null;
+		}
 
-        return this._grid[x, y].Comp;
-    }
+		return this._grid[x, y].Comp;
+	}
 
-    public void placeComponent(int x, int y, int orientation, ComponentItemPrototype prototype)
-    {
-        if (this.checkCollision(x, y, prototype.SizeX, prototype.SizeY))
-        {
-            // Out of Bounds
-            return;
-        }
+	public void placeComponent(int x, int y, int orientation, ComponentItemPrototype prototype)
+	{
+		if (this.checkCollision(x, y, prototype.SizeX, prototype.SizeY))
+		{
+			// Out of Bounds
+			return;
+		}
 
-        ComponentItem item = new ComponentItem(prototype);
+		ComponentItem item = new ComponentItem(prototype);
 
-        for (int i = 0; i < prototype.SizeX; i++)
-        {
-            for (int j = 0; j < prototype.SizeY; j++)
-            {
-                this._grid[x + i, y + j].Comp = item;
-                this._grid[x + i, y + j].CompOrientation = orientation;
-                this._grid[x + i, y + j].CompOffsetX = i;
-                this._grid[x + i, y + j].CompOffsetY = j;
-            }
-        }
-    }
+		for (int i = 0; i < prototype.SizeX; i++)
+		{
+			for (int j = 0; j < prototype.SizeY; j++)
+			{
+				this._grid[x + i, y + j].Comp = item;
+				this._grid[x + i, y + j].CompOrientation = orientation;
+				this._grid[x + i, y + j].CompOffsetX = i;
+				this._grid[x + i, y + j].CompOffsetY = j;
+			}
+		}
+	}
 
-    public void removeComponentAt(int x, int y)
-    {
-        ComponentItem item = this.getComponentAt(x, y);
+	public void removeComponentAt(int x, int y)
+	{
+		ComponentItem item = this.getComponentAt(x, y);
 
-        if (item == null)
-        {
-            return;
-        }
+		if (item == null)
+		{
+			return;
+		}
 
-        for (int i = 0; i < item.SizeX; i++)
-        {
-            for (int j = 0; j < item.SizeY; j++)
-            {
-                this._grid[x + i, y + j].Comp = null;
-                this._grid[x + i, y + j].CompOrientation = 0;
-                this._grid[x + i, y + j].CompOffsetX = 0;
-                this._grid[x + i, y + j].CompOffsetY = 0;
-            }
-        }
-    }
+		for (int i = 0; i < item.SizeX; i++)
+		{
+			for (int j = 0; j < item.SizeY; j++)
+			{
+				this._grid[x + i, y + j].Comp = null;
+				this._grid[x + i, y + j].CompOrientation = 0;
+				this._grid[x + i, y + j].CompOffsetX = 0;
+				this._grid[x + i, y + j].CompOffsetY = 0;
+			}
+		}
+	}
 }
