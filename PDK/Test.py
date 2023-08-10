b3a46687eec7b2fc263edb0927aba8ea575fdbf1
Michael Kissner <m1o1d1@gmail.com>
2023-08-10 09:16:16 +0200

    Querschnitt GDS

--- PDK/Test.py
+++ PDK/Test.py
@@ -8,8 +8,10 @@ def FullDesign(layoutName):
 
         grating = CAPICPDK.placeGrating_East(8).put(0, 0)
 
-        cell_00 = CAPICPDK.placeCell_StraightWG().put('west', grating.pin['io0'])
-        cell_01 = CAPICPDK.placeCell_StraightWG().put('west', cell_00.pin['east'])
+# -------------------
+
+        cell_00 = CAPICPDK.placeCell_StraightWG().put('west', grating.pin['io5'])
+        cell_01 = CAPICPDK.placeCell_Crossing().put('west', cell_00.pin['east'])
         cell_02 = CAPICPDK.placeCell_BendWG().put('west', cell_01.pin['east'])
         cell_03 = CAPICPDK.placeCell_StraightWG().put('west', cell_02.pin['south'])
         cell_04 = CAPICPDK.placeCell_BendWG().put('south', cell_03.pin['east'])
@@ -21,6 +23,17 @@ def FullDesign(layoutName):
         cell_10 = CAPICPDK.placeCell_StraightWG().put('west', cell_09.pin['east'])
         cell_11 = CAPICPDK.placeCell_StraightWG().put('west', cell_10.pin['east'])
         
+        cell_12 = CAPICPDK.placeCell_AWG().put('west', cell_01.pin['north'])
+        cell_13 = CAPICPDK.placeCell_Termination().put('west', cell_12.pin['east0'])
+        cell_14 = CAPICPDK.placeCell_DirectionalCoupler(deltaLength = 50).put('west0', cell_12.pin['east1'])
+        cell_15 = CAPICPDK.placeCell_Ring(deltaLength = 50).put('west0', cell_14.pin['east1'])
+        
+        cell_16 = CAPICPDK.placeCell_GratingCoupler().put('west', cell_01.pin['south'])
+        
+        cell_17 = CAPICPDK.placeCell_BendWG().put('south', cell_14.pin['east0'])
+        cell_18 = CAPICPDK.placeCell_StraightWG().put('west', cell_17.pin['west'])
+# -------------------
+
     return fullLayoutInner
 
 nd.print_warning = False
