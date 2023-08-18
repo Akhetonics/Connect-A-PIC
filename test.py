8a4318ca9d71610caa7b7049397d5e34c7b8499f
Max Aigner <max_aigneraigner@web.de>
2023-08-18 16:00:49 +0200

    fixed bug with drag n drop and attached the toolbox to the right side of the screen

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ def FullDesign(layoutName):
 
         grating = CAPICPDK.placeGratingArray_East(8).put(0, 0)
 
+        cell_0_1 = CAPICPDK.placeCell_StraightWG().put('west', grating.pin['io1'])
+        cell_1_1 = CAPICPDK.placeCell_StraightWG().put('west', cell_0_1.pin['east'])
+        cell_2_1 = CAPICPDK.placeCell_StraightWG().put('west', cell_1_1.pin['east'])
     return fullLayoutInner
 
 nd.print_warning = False
