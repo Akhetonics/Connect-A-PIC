233683f34b5cd203b21f2ffd1693958985604926
Max Aigner <max.aigner@shk.com>
2023-08-25 12:39:17 +0200

    Added Draft of loadingfunction to ComponentFactory.cs fixed unittest so that it can now convert the Dict<string,object> Object to yaml and then to the SBB object again

--- UnitTests/SBBBuilderTests.cs
+++ UnitTests/SBBBuilderTests.cs
@@ -37,50 +37,51 @@ namespace UnitTests
                     PdkLicense = null
                     }
                 },
-                { "blocks", new List<LabelBlock>
+                { "blocks", new Dictionary<string, LabelBlock>
                 {
-                    new LabelBlock
-                    {
-                        ID = "sampleID",
-                        Version = "1.0",
-                        License = "Sample License",
-                        CellName = "SampleCell",
-                        Doc = "This is a sample block.",
-                        BBox = new List<List<string>>
+                    { "SampleCell", new LabelBlock
                         {
-                            new List<string> { "0", "0" },
-                            new List<string> { "10", "0" },
-                            new List<string> { "10", "10" },
-                            new List<string> { "0", "10" }
-                        },
-                        BBMetalOutline = new List<List<List<string>>>
-                        {
-                            new List<List<string>>
+                            ID = "sampleID",
+                            Version = "1.0",
+                            License = "Sample License",
+                            CellName = "SampleCell",
+                            Doc = "This is a sample block.",
+                            BBox = new List<List<string>>
                             {
-                                new List<string> { "1", "1" },
-                                new List<string> { "9", "1" },
-                                new List<string> { "9", "9" },
-                                new List<string> { "1", "9" }
-                            }
-                        },
-                        BBWidth = 10.0f,
-                        BBLength = 10.0f,
-                        PinIn = "InputPin",
-                        PinOut = "OutputPin",
-                        Pins = new Dictionary<string, LabelPin>
-                        {
-                            { "pin1", new LabelPin() } // Assuming a default constructor or dummy values for LabelPin
-                        },
-                        Parameters = new Dictionary<string, LabelParameter>
-                        {
-                            { "param1", new LabelParameter() } // Assuming a default constructor or dummy values for LabelParameter
-                        },
-                        KeywordParameters = new List<string> { "keyword1", "keyword2" },
-                        CellNameParameters = new List<string> { "cellnameParam1" },
-                        Call = "SampleCallFunction",
-                        GroupName = "SampleGroup",
-                        IPBlock = new LabelIPBlock(), // Assuming a default constructor or dummy values for LabelIPBlock
-                        Icon = new LabelIcon() // Assuming a default constructor or dummy values for LabelIcon
+                                new List<string> { "0", "0" },
+                                new List<string> { "10", "0" },
+                                new List<string> { "10", "10" },
+                                new List<string> { "0", "10" }
+                            },
+                            BBMetalOutline = new List<List<List<string>>>
+                            {
+                                new List<List<string>>
+                                {
+                                    new List<string> { "1", "1" },
+                                    new List<string> { "9", "1" },
+                                    new List<string> { "9", "9" },
+                                    new List<string> { "1", "9" }
+                                }
+                            },
+                            BBWidth = 10.0f,
+                            BBLength = 10.0f,
+                            PinIn = "InputPin",
+                            PinOut = "OutputPin",
+                            Pins = new Dictionary<string, LabelPin>
+                            {
+                                { "pin1", new LabelPin() } // Assuming a default constructor or dummy values for LabelPin
+                            },
+                            Parameters = new Dictionary<string, LabelParameter>
+                            {
+                                { "param1", new LabelParameter() } // Assuming a default constructor or dummy values for LabelParameter
+                            },
+                            KeywordParameters = new List<string> { "keyword1", "keyword2" },
+                            CellNameParameters = new List<string> { "cellnameParam1" },
+                            Call = "SampleCallFunction",
+                            GroupName = "SampleGroup",
+                            IPBlock = new LabelIPBlock(), // Assuming a default constructor or dummy values for LabelIPBlock
+                            Icon = new LabelIcon() // Assuming a default constructor or dummy values for LabelIcon
+                        }
                     }
                 }
             } };
@@ -88,7 +89,7 @@ namespace UnitTests
             var yamlSerializer = new SerializerBuilder().Build();
             var yaml = yamlSerializer.Serialize(BasicComponent);
             SBB sbb = SBBBuilder.createSBB(yaml);
-            Assert.Equal("sampleID", sbb.Blocks.First(b=>b.Value.CellName== "SampleCell").Value.ID);
+            Assert.Equal("sampleID", sbb.Blocks.First(b => b.Value.CellName == "SampleCell").Value.ID);
         }
     }
 }
