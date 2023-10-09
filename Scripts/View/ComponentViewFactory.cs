34989959b5259ff7aea40533e78ea821827a09d5
Max Aigner <max.aigner@shk.com>
2023-10-09 16:07:14 +0200

    Todo rewrite componentViewFactory

--- Scripts/View/ComponentViewFactory.cs
+++ Scripts/View/ComponentViewFactory.cs
@@ -1,13 +1,16 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ConnectAPIC.LayoutWindow.View
 {
 	public partial class ComponentViewFactory : Node
 	{
-		[Export] public PackedScene[] PackedComponentScenes;
+		private List<PackedScene> PackedComponentScenes;
 		private static ComponentViewFactory instance { get; set; }
+		private Dictionary<Type, ComponentBaseView> ComponentViewCache = new Dictionary<Type, ComponentBaseView>();
 		public static ComponentViewFactory Instance
 		{
 			get { return instance; }
@@ -16,25 +19,55 @@ namespace ConnectAPIC.LayoutWindow.View
 		public override void _Ready()
 		{
 			if (instance == null)
-			{
-				instance = this;
-			}
-			else
+            {
+                instance = this;
+                PackedComponentScenes = new List<PackedScene>();
+                var folderPath = "Scenes\\Components";
+                string[] sceneFiles = Directory.GetFiles(folderPath, "*.tscn", SearchOption.AllDirectories);
+                foreach (string sceneFile in sceneFiles)
+                {
+                    string godotPath = sceneFile.Replace(folderPath, "res://Scenes/Components").Replace("\\", "/");
+                    PackedScene scene = GD.Load<PackedScene>(godotPath);
+                    PackedComponentScenes.Add(scene);
+                }
+                CachePackedSceneTypes();
+            }
+            else
 			{
 				QueueFree(); // delete this object as there is already another GameManager in the scene
 				return;
 			}
 		}
 
-		public ComponentBaseView CreateComponentView(Type ViewTypeListedInFactoryChildren)
+        private void CachePackedSceneTypes()
+        {
+            ComponentViewCache = new Dictionary<Type, ComponentBaseView>();
+            foreach (PackedScene componentTemplate in PackedComponentScenes)
+            {
+                ComponentBaseView mainNode = componentTemplate.Instantiate() as ComponentBaseView;
+                if (mainNode == null)
+                {
+                    CustomLogger.PrintErr($"ComponentTemplate is not of type ComponentBaseView: {componentTemplate.ResourcePath}");
+                    continue;
+                }
+                ComponentViewCache.Add(mainNode.GetType(), mainNode);
+                mainNode.QueueFree();
+            }
+        }
+
+        public ComponentBaseView CreateComponentView(Type ViewTypeListedInFactoryChildren)
 		{
 			if (! typeof(ComponentBaseView).IsAssignableFrom(ViewTypeListedInFactoryChildren)){
 				CustomLogger.PrintErr($"Type is not of ComponentBaseView: {nameof(ViewTypeListedInFactoryChildren) + " " + ViewTypeListedInFactoryChildren.FullName}");
                 throw new ArgumentException(nameof(ViewTypeListedInFactoryChildren));
 			}
-			foreach (PackedScene componentTemplate in PackedComponentScenes)
+
+			ComponentViewCache.Single(c => c.Key == ViewTypeListedInFactoryChildren);
+
+            foreach (var componentCacheElement in ComponentViewCache)
 			{
-                ComponentBaseView mainNode = componentTemplate.Instantiate() as ComponentBaseView;
+                componentCacheElement)
+                ComponentBaseView mainNode = componentCacheElement.Instantiate() as ComponentBaseView;
 				if(mainNode == null)
 				{
 					CustomLogger.PrintErr($"ComponentTemplate is not of type ComponentBaseView: {componentTemplate.ResourcePath}");
@@ -43,6 +76,9 @@ namespace ConnectAPIC.LayoutWindow.View
 				if (ViewTypeListedInFactoryChildren == mainNode.GetType())
 				{
 					return mainNode;
+				} else
+				{
+					mainNode.QueueFree();
 				}
 			}
 			CustomLogger.PrintErr($"ComponentTemplate is not defined: {ViewTypeListedInFactoryChildren.FullName}");
