58fc8f1d9a9a84ce3e328b1e3b68356576a350b2
Max Aigner <max_aigneraigner@web.de>
2023-10-20 13:13:22 +0200

    saved new directionalcoupler trial - however there is the problem that it consists of more than one tile so we have to give the shader the information about multi-tile components so it does not stretch the overlay over the whole compnoent.

--- Scenes/Components/DirectionalCoupler/DirectionalCouplerView.cs
+++ Scenes/Components/DirectionalCoupler/DirectionalCouplerView.cs
@@ -16,18 +16,18 @@ namespace ConnectAPIC.LayoutWindow.View
 		[Export] private Texture LightFlowOverlayRightUpIn;
 		[Export] private Texture LightFlowOverlayRightDownIn;
 
-        public override void InitializeAnimationSlots()
-        {
-            this.CheckForNull(x => x.LightFlowOverlayLeftUpIn);
-            this.CheckForNull(x => x.LightFlowOverlayLeftDownIn);
-            this.CheckForNull(x => x.LightFlowOverlayRightUpIn);
-            this.CheckForNull(x => x.LightFlowOverlayRightDownIn);
-            var textOffsetX = GameManager.TilePixelSize;
-            AnimationSlots.AddRange(CreateRGBAnimSlots(RectSide.Left, LightFlowOverlayLeftUpIn));
-            AnimationSlots.AddRange(CreateRGBAnimSlots(RectSide.Left, LightFlowOverlayLeftDownIn,0,1));
-            AnimationSlots.AddRange(CreateRGBAnimSlots(RectSide.Left, LightFlowOverlayRightUpIn,1,0,new Vector2()));
-            AnimationSlots.AddRange(CreateRGBAnimSlots(RectSide.Left, LightFlowOverlayRightDownIn));
-        }
+		public override void InitializeAnimationSlots()
+		{
+			this.CheckForNull(x => x.LightFlowOverlayLeftUpIn);
+			this.CheckForNull(x => x.LightFlowOverlayLeftDownIn);
+			this.CheckForNull(x => x.LightFlowOverlayRightUpIn);
+			this.CheckForNull(x => x.LightFlowOverlayRightDownIn);
+			var textOffsetX = GameManager.TilePixelSize;
+			AnimationSlots.AddRange(CreateRGBAnimSlots(RectSide.Left, LightFlowOverlayLeftUpIn));
+			AnimationSlots.AddRange(CreateRGBAnimSlots(RectSide.Left, LightFlowOverlayLeftDownIn,0,1));
+			AnimationSlots.AddRange(CreateRGBAnimSlots(RectSide.Left, LightFlowOverlayRightUpIn,1,0,new Vector2()));
+			AnimationSlots.AddRange(CreateRGBAnimSlots(RectSide.Left, LightFlowOverlayRightDownIn));
+		}
 
-    }
+	}
 }
