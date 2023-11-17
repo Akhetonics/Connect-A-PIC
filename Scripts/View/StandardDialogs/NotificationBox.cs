821ec9c36dc3c01a1125f17fb2b91ec2699f03af
Max Aigner <max_aigneraigner@web.de>
2023-11-17 14:10:29 +0100

    fixed csproj file so that it runs again

--- Scripts/View/StandardDialogs/NotificationBox.cs
+++ Scripts/View/StandardDialogs/NotificationBox.cs
@@ -1,13 +1,8 @@
 using Godot;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConnectAPIC.LayoutWindow.View
 {
-	public partial class NotificationBox : Control
+    public partial class NotificationBox : Control
 	{
 		private Label messageLabel;
 		private Timer timer;
