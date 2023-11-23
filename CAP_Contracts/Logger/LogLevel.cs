0e46ef02c4c1ff82bd0b9fbf1f4c165ab56a8500
Max Aigner <max_aigneraigner@web.de>
2023-11-23 14:22:24 +0100

    added the connect a PIC contracts project that has all the interfaces for all the projects to the git branch - accidentally it was not yet.

---
+++ CAP_Contracts/Logger/LogLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace CAP_Contracts.Logger
+{
+    public enum LogLevel
+    {
+        Trace,  // Very detailed information, mainly for developers
+        Debug,  // Information that is useful for diagnosing issues
+        Info,   // General runtime information
+        Warn,   // Something unusual happened, but the application continues to run
+        Error,  // An error occurred that affects the execution of a function
+        Fatal   // A critical error that can cause the application to crash
+    }
+}
