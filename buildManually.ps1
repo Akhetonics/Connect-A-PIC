16419e86f23bd24e75b58a572bfac1f1f645f3d1
aignermax <max_aigneraigner@web.de>
2023-12-12 16:45:08 +0100

    experimenting with build script

--- buildManually.ps1
+++ buildManually.ps1
@@ -1,2 +1,48 @@
-$godotProcess = Start-Process -FilePath $Env:GODOT -ArgumentList "--headless", "--export-release", "`"Windows Desktop`"" -PassThru
+# Adjust the Godot path and arguments as per your local setup
+$godotPath = $env:GODOT
+
+# Build Windows EXE
+$godotArgs = "--headless", "--export-release", "`"Windows Desktop`""
+$godotProcess = Start-Process -FilePath $godotPath -ArgumentList $godotArgs -PassThru
 $godotProcess.WaitForExit()
+
+Write-Host "Press any key to continue..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+exit 
+# Extract Version from Project File
+$version = Select-String -Path "Connect-A-PIC.csproj" -Pattern '<Version>(.*)</Version>' | % { $_.Matches.Groups[1].Value }
+Write-Host "The extracted project version is $version"
+
+# Read Godot version from global.json
+$godotVersion = Get-Content -Raw -Path 'global.json' | ConvertFrom-Json | Select-Object -ExpandProperty 'msbuild-sdks' | Select-Object -ExpandProperty 'Godot.NET.Sdk'
+Write-Host "Godot version: $godotVersion"
+
+# Adjust the Godot path and arguments as per your local setup
+$godotPath = $env:GODOT
+
+# Delete all old Releases
+Remove-Item -Path "Release\*" -Recurse -Force
+Write-Host "Release Folder got cleared"
+
+# Create Build Directory
+$releaseDir = "Release"
+if (-not (Test-Path $releaseDir)) {
+    New-Item -ItemType Directory -Path $releaseDir
+}
+
+# Build Windows EXE
+$godotArgs = "--headless", "--export-release", "`"Windows Desktop`""
+$godotProcess = Start-Process -FilePath $godotPath -ArgumentList $godotArgs -PassThru
+$godotProcess.WaitForExit()
+
+# Zip Release Build
+$zipPath = Join-Path "$releaseDir" "ConnectAPIC.zip"
+Compress-Archive -Path "$releaseDir\*" -DestinationPath $zipPath
+
+# List files in Release Directory
+Write-Host "list files in folder $releaseDir"
+Get-ChildItem -Path $releaseDir -Recurse | ForEach-Object { Write-Host $_.FullName }
+
+
+Write-Host "Press any key to continue..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
