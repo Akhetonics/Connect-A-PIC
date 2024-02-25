c8e131e7aabb126def11810a0d100b078a7e67b1
Cx3n1 <lukagobronidze@gmail.com>
2024-02-25 14:39:33 +0400

    Working on ExternalPortViewModel, making it work with new flexible external ports and combining it with PowerMeterViewModel.

--- Scenes/ExternalInputs/NewAssets/ExternalPortView.cs
+++ Scenes/ExternalInputs/NewAssets/ExternalPortView.cs
@@ -1,4 +1,5 @@
 using CAP_Core.ExternalPorts;
+using ConnectAPIC.Scripts.ViewModel;
 using Godot;
 using MathNet.Numerics.Distributions;
 using System;
@@ -6,7 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 
 
-namespace ConnectAPIC.Scenes.ExternalInputs
+namespace ConnectAPIC.Scenes.ExternalPorts
 {
     public partial class ExternalPortView : Node2D
     {
@@ -14,15 +15,37 @@ namespace ConnectAPIC.Scenes.ExternalInputs
         [Export] public Texture2D InputTexture { set; get; }
         [Export] public Texture2D OutputTexture { set; get; }
 
+        public ExternalPortViewModel ViewModel { get; set; }
 
-        private List<PointLight2D> lightContainer;
-        private TextureRect currentTexture;
+
+        List<PointLight2D> lightContainer;
+        TextureRect currentTexture;
+        RichTextLabel InfoLabel;
         bool isInput = false;
         bool lightsOn = true;
         float pulseValue = 0;
         float minPulseEnergy = 0.8f;
         float maxPulseEnergy = 1.2f;
 
+
+        public ExternalPortView(ExternalPortViewModel viewModel) {
+            ViewModel = viewModel;
+
+            ViewModel.LightChanged += (object sender, bool e) =>
+            {
+                if (isInput) {
+                    if (e)
+                        TurnOn();
+                    else
+                        TurnOff();
+                }
+            };
+
+            viewModel.PowerChanged += (object sender, string e) =>
+            {
+                if (!isInput) DisplayPower(e);
+            };
+        }
 
         public override void _Ready()
         {
@@ -34,10 +57,7 @@ namespace ConnectAPIC.Scenes.ExternalInputs
                 lightContainer.Add(light);
             }
 
-            pulseValue = (float)(new Random().NextDouble() * 10);
-
             SetAsOutput();
-            SetAsInput(true, 0, 1, 0);
         }
 
         public override void _Process(double delta)
@@ -48,60 +68,74 @@ namespace ConnectAPIC.Scenes.ExternalInputs
             }
         }
 
-        public void SetAsOutput()
+
+        public ExternalPortView SetAsOutput()
         {
             isInput = false;
             currentTexture.Texture = OutputTexture;
             TurnOff();
+
+            return this;
         }
 
-        public void SetAsInput(bool lightsOn, float red, float green, float blue, float alpha = 1)
+        public ExternalPortView SetAsInput(float red, float green, float blue, float alpha = 1)
         {
             isInput = true;
+            lightsOn = false;
             currentTexture.Texture = InputTexture;
+            pulseValue = (float)(new Random().NextDouble() * 10);
+
+            TurnOff();
             SetLightColor(red, green, blue, alpha);
 
-            if (lightsOn)
-            {
-                TurnOn();
-            }
-            else
-            {
-                TurnOff();
-            }
+            return this;
         }
 
-        public void TurnOff()
+        public ExternalPortView TurnOff()
         {
             if (lightsOn)
             {
                 lightContainer.ForEach((PointLight2D x) => x.Enabled = false);
                 lightsOn = false;
             }
+
+            return this;
         }
 
-        public void TurnOn()
+        public ExternalPortView TurnOn()
         {
-            if (!lightsOn && isInput)
+            if (isInput)
             {
                 lightContainer.ForEach((PointLight2D x) => x.Enabled = true);
                 lightsOn = true;
             }
+
+            return this;
         }
 
         /// <summary>
         /// sets color of input pin according to RGBA colors (in range 0-1)
+        /// and returns ExternalPortView with the assigned light color
         /// </summary>
         /// <param name="red">float value between 0-1 indicating red</param>
         /// <param name="green">float value between 0-1 indicating green</param>
         /// <param name="blue">float value between 0-1 indicating blue</param>
         /// <param name="alpha">float value between 0-1 indicating transparency</param>
-        public void SetLightColor(float red, float green, float blue, float alpha = 1)
+        /// <returns>ExternalPortView - with lighting of the given type</returns>
+        public ExternalPortView SetLightColor(float red, float green, float blue, float alpha = 1)
         {
             for (int i = 0; i < lightContainer.Count; i++)
             {
                 lightContainer[i].Color = new Color(red, green, blue, alpha);
             }
+
+            return this;
+        }
+
+
+        private void DisplayPower(string labelText)
+        {
+            InfoLabel.Text = labelText;
         }
 
         private void Pulsate(double delta)
