66ac75a45a308cb7959ab71dd23fb4ae1f9689d4
aignermax <max_aigneraigner@web.de>
2024-02-26 11:10:27 +0100

    fixing error messaging in templatetileview

--- Scripts/View/TemplateTileView.cs
+++ Scripts/View/TemplateTileView.cs
@@ -1,35 +1,41 @@
+using CAP_Contracts.Logger;
+using Chickensoft.AutoInject;
 using ConnectAPic.LayoutWindow;
 using ConnectAPIC.Scripts.Helpers;
 using Godot;
+using SuperNodes.Types;
 using System;
 
 namespace ConnectAPIC.LayoutWindow.View
 {
+    [SuperNode(typeof(Dependent))]
     public partial class TemplateTileView : TextureRect
     {
-        [Export] public ComponentViewFactory componentViewFactory { get; set; }
+        //[Export] public ComponentViewFactory componentViewFactory { get; set; }
+        public override partial void _Notification(int what);
+        [Dependency] public ComponentViewFactory ComponentViewFactory => DependOn<ComponentViewFactory>();
+        [Dependency] public ILogger Logger => DependOn<ILogger>();
         public override void _Ready()
         {
             base._Ready();
-            this.CheckForNull(x => x.componentViewFactory);
         }
         public override void _DropData(Vector2 position, Variant data)
         { // you cannot drop something on a template tile
         }
 
-		public override Variant _GetDragData(Vector2 position)
-		{
-			if( GetChild(0) is ComponentView template)
-			{
-				var newComponent = componentViewFactory.CreateComponentView(template.ViewModel.TypeNumber);
-				return newComponent;
-			} else
-			{
-				var exceptionText = "child of _GetDragData() is not of type ComponentView even though it should be";
-                GD.PrintErr(exceptionText);
-				throw new DataMisalignedException(exceptionText);
-			}
-		}
+        public override Variant _GetDragData(Vector2 position)
+        {
+            if (GetChild(0) is ComponentView template)
+            {
+                var newComponent = ComponentViewFactory.CreateComponentView(template.ViewModel.TypeNumber);
+                return newComponent;
+            } else
+            {
+                var exceptionText = "child of _GetDragData() is not of type ComponentView even though it should be";
+                Logger.PrintErr(exceptionText);
+                throw new DataMisalignedException(exceptionText);
+            }
+        }
 
         public override void _GuiInput(InputEvent inputEvent)
         {
