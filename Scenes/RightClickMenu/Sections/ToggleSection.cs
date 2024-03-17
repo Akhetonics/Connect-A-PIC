9fa60a1bb67d807f72350ab2d3c92bc1f88bf488
Cx3n1 <lukagobronidze@gmail.com>
2024-03-17 15:32:33 +0400

    Finished right click menu sections.

--- Scenes/RightClickMenu/Sections/ToggleSection.cs
+++ Scenes/RightClickMenu/Sections/ToggleSection.cs
@@ -1,15 +1,49 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 public partial class ToggleSection : ISection
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    private List<String> toggleValues;
+    private int toggleIndex
+    {
+        get => toggleIndex;
+        set
+        {
+            toggleIndex = value%toggleValues.Count;
+            Value = toggleValues[toggleIndex];
+        }
+    }
+
+    public void Initialize(List<String> toggleValues, String title, String value = "")
+    {
+        Title = title;
+        Value = value;
+
+        if (toggleValues == null) return;
+        this.toggleValues = toggleValues;
+
+        if (Value.Equals(""))
+            Value = toggleValues[0];
+    }
+
+    public void CycleToNextValue(){
+        toggleIndex++;
+    }
+
+    public String GetNextToggleValue()
+    {
+        return toggleValues[(toggleIndex + 1) % toggleValues.Count];
+    }
+
+    private void OnToggleButtonPressed()
 	{
-	}
+        // We send that we intedn to toggle to next value
+        OnPropertyChanged(this, new PropertyChangedEventArgs(GetNextToggleValue()));
+    }
 }
+
+
+
