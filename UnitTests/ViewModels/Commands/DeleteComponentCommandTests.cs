7c70a085d95337011516a56f099f02588badc0ae
Cx3n1 <lukagobronidze@gmail.com>
2024-04-18 18:37:49 +0400

    WIP - wrote delete component command test, command components need some work to make this test pass.

--- UnitTests/ViewModels/Commands/DeleteComponentCommandTests.cs
+++ UnitTests/ViewModels/Commands/DeleteComponentCommandTests.cs
@@ -62,45 +62,69 @@ namespace UnitTests.ViewModels.Commands
         [Fact]
         public void CanExecute_ReturnsFalse_WhenCreateIsInvalid()
         {  
-            //gridManagerMock.Object.ComponentMover.PlaceComponent(1, 0, TestComponentFactory.CreateComponent(TestComponentFactory.StraightWGJson));
+            //// arrange
+            //var onEmptyTile = new List<IntVector>() {
+            //    new IntVector(1,0)
+            //};
+            //var outOfGrid = new List<IntVector>() {
+            //    new IntVector(-1,0)
+            //};
 
-            //TODO: need to use intVectors instead of coordinates
-            // arrange
-            var argsOnEmptyTile = new DeleteComponentArgs(0, 1, 0, DiscreteRotation.R0);
-            var argsOutOfGrid = new CreateComponentArgs(0, -1, 0, DiscreteRotation.R0);
+            //var argsOnEmptyTile = new DeleteComponentArgs(onEmptyTile);
+            //var argsOutOfGrid = new DeleteComponentArgs(outOfGrid);
 
-            // act
-            var canExecuteOnAnotherComponent = command.CanExecute(argsOnOtherComponent);
-            var canExecuteOutOfGrid = command.CanExecute(argsOutOfGrid);
+            //// act
+            //var canExecuteOnEmptyTile = command.CanExecute(argsOnEmptyTile);
+            //var canExecuteOutOfGrid = command.CanExecute(argsOutOfGrid);
 
-            // assert
-            Assert.False(canExecuteOnAnotherComponent);
-            Assert.False(canExecuteOutOfGrid);
+            //// assert
+            //Assert.False(canExecuteOnEmptyTile);
+            //Assert.False(canExecuteOutOfGrid);
         }
 
         [Fact]
         public async Task TestExecute() {
+            gridManagerMock.Object.ComponentMover.PlaceComponent(1, 0, TestComponentFactory.CreateComponent(TestComponentFactory.StraightWGJson));
+            gridManagerMock.Object.ComponentMover.PlaceComponent(2, 0, TestComponentFactory.CreateComponent(TestComponentFactory.StraightWGJson));
+            gridManagerMock.Object.ComponentMover.PlaceComponent(3, 0, TestComponentFactory.CreateComponent(TestComponentFactory.StraightWGJson));
+            gridManagerMock.Object.ComponentMover.PlaceComponent(5, 0, TestComponentFactory.CreateComponent(TestComponentFactory.MMI3x3));
+
             // arrange
-            var args = new CreateComponentArgs(0, 4, 4, DiscreteRotation.R0);
+            var onSingleComponent = new List<IntVector>() {
+                new IntVector(1,0)
+            };
+            var onMultipleComponents = new List<IntVector>() {
+                new IntVector(2,0), new IntVector(3,0)
+            };
+            var onBigComponent = new List<IntVector>() {
+                new IntVector(6,1)
+            };
+
+            var argsOnSingleComponent = new DeleteComponentArgs(onSingleComponent);
+            var argsOnMultipleComponents = new DeleteComponentArgs(onMultipleComponents);
+            var argsOnBigComponent = new DeleteComponentArgs(onBigComponent);
 
             // act
-            await command.ExecuteAsync(args);
+            await command.ExecuteAsync(argsOnMultipleComponents);
+            //TODO: for this need better component checking in component mover
+            //await command.ExecuteAsync(argsOnBigComponent);
+            await command.ExecuteAsync(argsOnSingleComponent);
 
-            bool componentPlaced = gridManagerMock.Object.TileManager.Tiles[4, 4].Component != null;
 
+            bool deletedSingleComponent = gridManagerMock.Object.TileManager.Tiles[1, 0].Component == null;
+            bool deletedMultiple = gridManagerMock.Object.TileManager.Tiles[2, 0].Component == null
+                                    && gridManagerMock.Object.TileManager.Tiles[2, 1].Component == null;
+            bool deletedBigComponent = gridManagerMock.Object.TileManager.Tiles[5, 0].Component == null;
+
+            // undoes single deleted component
             command.Undo();
 
             // assert
-            Assert.True(componentPlaced);
-            Assert.True(gridManagerMock.Object.TileManager.Tiles[4, 4].Component == null);
-        }
-
-        private static ComponentFactory InitializeComponentFactory() {
-            var componentDrafts = new List<Component>() { TestComponentFactory.CreateComponent(TestComponentFactory.StraightWGJson) };
-            var componentFactory = new ComponentFactory();
-            componentFactory.InitializeComponentDrafts(componentDrafts);
-            return componentFactory;
+            Assert.True(deletedSingleComponent);
+            Assert.True(deletedMultiple);
+            //TODO: for this need better component checking in component mover
+            //Assert.True(deletedBigComponent);
+            Assert.True(gridManagerMock.Object.TileManager.Tiles[1, 0].Component != null);
         }
     }
-
 }
