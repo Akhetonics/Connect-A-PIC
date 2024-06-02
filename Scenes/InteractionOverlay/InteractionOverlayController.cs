574411180aefe2d6cc418e8a6ee9b662420aaf7a
Cx3n1 <lukagobronidze@gmail.com>
2024-06-02 22:50:51 +0400

    WIP - Making overlay work in godot with collision shapes

--- Scenes/InteractionOverlay/InteractionOverlayController.cs
+++ Scenes/InteractionOverlay/InteractionOverlayController.cs
@@ -1,3 +1,4 @@
+using Godot;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,20 +8,133 @@ using System.Threading.Tasks;
 
 namespace ConnectAPIC.Scenes.InteractionOverlay
 {
-    internal class InteractionOverlayController
+    public static class InteractionOverlayController
     {
-        public List<OverlayElement> ObservedElements;
-        public List<OverlayElement> ActiveElements;
+        /// <summary>
+        /// Default value for scrolling (this value will be chosen if mouse cursor is outside of every element,
+        ///     or if mouse behaviour is cancelled out by elements, like in case of 2 elements on the same z index
+        ///     where one restricts scrolling and other allows it then default value will be chosen)
+        /// </summary>
+        public static bool DefaultScrollValue { set; get; } = true;
 
-        public int CurrentMaxZIndex {  set; get; }
-        public bool ScrollingAllowed { set; get; }
-        public bool ClikcingAllowed { set; get; }
+        /// <summary>
+        /// Default value for clicking (this value will be chosen if mouse cursor is outside of every element,
+        ///     or if mouse behaviour is cancelled out by elements, like in case of 2 elements on the same z index
+        ///     where one restricts clicking and other allows it then default value will be chosen)
+        /// </summary>
+        public static bool DefaultClickValue { set; get; } = true;
 
-        public Hashtable ScrollingPremissionByZIndex { set; get; } = new();
-        public Hashtable ClickingPremissionByZIndex { set; get; } = new();
+        public static bool ScrollingAllowed { set; get; }
+        public static bool ClickingAllowed { set; get; }
 
-        //TODO: add connect/disconnect functionallity
-        //TODO: figure oute hash tbles stuff
-        //TODO: Determine activity change (maybe emit from elements if their visibility changes and then remove them from active elements?)
+        private static bool IsMaxIndexSet { set; get; } = false;
+        private static int CurrentMaxZIndex { set; get; } = int.MinValue;
+
+        private static List<OverlayElement> ObservedElements { set; get; }
+        private static Dictionary<int, int> ScrollingPremissionByZIndex { set; get; } = new();
+        private static Dictionary<int, int> ClickingPremissionByZIndex { set; get; } = new();
+
+
+        public static void Connect(OverlayElement element){
+            if (ObservedElements.Contains(element)) return;
+            ObservedElements.Add(element);
+            element.AreaEntered += MouseEnteredElement;
+            element.AreaExited += MouseExitedElement;
+            if (element.MouseInsideAreas)
+                MouseEnteredElement(element, element);
+        }
+
+        public static void Disconnect(OverlayElement element){
+            ObservedElements.Remove(element);
+            MouseExitedElement(element, element);
+            element.AreaEntered -= MouseEnteredElement;
+            element.AreaExited -= MouseExitedElement;
+        }
+
+        public static void MouseEnteredElement(object sender, OverlayElement element){
+            AddPremissionValuesOfElement(element);
+
+            if (CurrentMaxZIndex < element.OverlayZIndex)
+            {
+                CurrentMaxZIndex = element.OverlayZIndex;
+                IsMaxIndexSet = true;
+            }
+
+            ApplyMouseBehaviourByMaxIndex();
+        }
+
+        public static void MouseExitedElement(object sender, OverlayElement element)
+        {
+            RemovePremissionValuesOfElement(element);
+            SetNewMaxIndex();
+            ApplyMouseBehaviourByMaxIndex();
+        }
+
+        private static void SetNewMaxIndex()
+        {
+            if (ScrollingPremissionByZIndex.Count == 0) // scroling and clicking premission dictionaries are equal in size
+            {
+                IsMaxIndexSet = false;
+                CurrentMaxZIndex = int.MinValue;
+                return;
+            }
+
+            var zIndecies = ScrollingPremissionByZIndex.Keys;
+
+            CurrentMaxZIndex = zIndecies.Max();
+            IsMaxIndexSet = true;
+        }
+
+        private static void ApplyMouseBehaviourByMaxIndex()
+        {
+            if (!IsMaxIndexSet)
+            {
+                ScrollingAllowed = DefaultScrollValue;
+                ClickingAllowed = DefaultClickValue;
+                return;
+            }
+
+            var scrollVal = ScrollingPremissionByZIndex[CurrentMaxZIndex];
+            var clickVal = ClickingPremissionByZIndex[CurrentMaxZIndex];
+
+            if (scrollVal > 0) ScrollingAllowed = true;
+            else if (scrollVal < 0) ScrollingAllowed = false;
+            else ScrollingAllowed = DefaultScrollValue;
+
+            if (clickVal > 0) ClickingAllowed = true;
+            else if (clickVal < 0) ClickingAllowed = false;
+            else ClickingAllowed = DefaultClickValue;
+        }
+
+        private static void AddPremissionValuesOfElement(OverlayElement element){
+            var zIndex = element.OverlayZIndex;
+
+            if (!ScrollingPremissionByZIndex.ContainsKey(zIndex))
+                ScrollingPremissionByZIndex[zIndex] = 0;
+
+            ScrollingPremissionByZIndex[zIndex] += (element.Scrolling ? 1 : -1);
+
+            if (!ClickingPremissionByZIndex.ContainsKey(zIndex))
+                ClickingPremissionByZIndex[zIndex] = 0;
+
+            ClickingPremissionByZIndex[zIndex] += (element.Clicking ? 1 : -1);
+        }
+
+        private static void RemovePremissionValuesOfElement(OverlayElement element)
+        {
+            var zIndex = element.OverlayZIndex;
+
+            if (ScrollingPremissionByZIndex.ContainsKey(zIndex)){
+                ScrollingPremissionByZIndex[zIndex] -= (element.Scrolling ? 1 : -1);
+                if (ScrollingPremissionByZIndex[zIndex] == 0)
+                    ScrollingPremissionByZIndex.Remove(zIndex);
+            }
+
+            if (ClickingPremissionByZIndex.ContainsKey(zIndex)){
+                ClickingPremissionByZIndex[zIndex] -= (element.Clicking ? 1 : -1);
+                if (ClickingPremissionByZIndex[zIndex] == 0)
+                    ClickingPremissionByZIndex.Remove(zIndex);
+            }
+        }
     }
 }
--- Scenes/InteractionOverlay/OverlayElement.cs
+++ Scenes/InteractionOverlay/OverlayElement.cs
@@ -1,8 +1,9 @@
+using ConnectAPIC.Scenes.InteractionOverlay;
 using Godot;
 using System;
 using System.ComponentModel;
 
-public partial class OverlayElement : Control
+public partial class OverlayElement : Node2D
 {
     /// <summary>
     /// Determines if mouse scrolling is allowed inside the areas
@@ -29,14 +30,19 @@ public partial class OverlayElement : Control
     public Godot.Collections.Array<Area2D> InteractionAreas { get; set; }
 
     /// <summary>
+    /// Defines if mouse is inside any of the Interaction Areas
+    /// </summary>
+    public bool MouseInsideAreas { get; set; }
+
+    /// <summary>
     /// Invoked if mouse cursor enters any one of the areas
     /// </summary>
-    public event EventHandler AreaEntered;
+    public event EventHandler<OverlayElement> AreaEntered;
 
     /// <summary>
     /// Invoked when mouse cursor leaves all areas (is in no area from <see cref="InteractionAreas"/>)
     /// </summary>
-    public event EventHandler AreaExited;
+    public event EventHandler<OverlayElement> AreaExited;
 
 	public override void _Ready()
 	{
@@ -44,15 +50,28 @@ public partial class OverlayElement : Control
             area.MouseEntered += Area_MouseEntered;
             area.MouseExited += Area_MouseExited;
         }
+
+        this.VisibilityChanged += ActivationStatusChanged;
 	}
+
+    private void ActivationStatusChanged()
+    {
+        if (this.Visible) {
+            InteractionOverlayController.Connect(this);
+        } else {
+            InteractionOverlayController.Disconnect(this);
+        }
+    }
 
     private void Area_MouseExited()
     {
-        AreaEntered.Invoke(this, EventArgs.Empty);
+        AreaEntered.Invoke(this, this);
+        MouseInsideAreas = true;
     }
 
     private void Area_MouseEntered()
     {
-        AreaExited.Invoke(this, EventArgs.Empty);
+        AreaExited.Invoke(this, this);
+        MouseInsideAreas = false;
     }
 }
